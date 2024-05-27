5bc0db8cdc0cb1f7750848ba30996f4265f435f2
Justin Deschenaux <justin.deschenaux@gmail.com>
2024-05-27 14:37:56 +0200

    expose new fct

--- simple_cfg/__init__.py
+++ simple_cfg/__init__.py
@@ -1,1 +1,1 @@
-from .cfg import default_arguments as cli_defaults, add_args, add_module_args, parse_args, save_args_to_cfg
+from .cfg import default_arguments as cli_defaults, add_args, add_module_args, parse_args, save_args_to_cfg, get_parser
